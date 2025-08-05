9c4912397ff6c54794f31d4f71f742f7b57642cc
Benedikt Schächner <benedikt.schaechner@gmail.com>
2025-08-05 10:53:23 +0000

    .. Benedikt Schächner Download (1).jpeg Add files via upload now Benedi

--- src/lib/gallery.ts
+++ src/lib/gallery.ts
@@ -46,4 +46,76 @@ export const galleryData: GalleryImage[] = [
     description: 'Bereit für das nächste Event.',
     aiHint: 'person formal'
   },
+  {
+    src: 'https://raw.githubusercontent.com/SchBenedikt/portfolio/master/src/app/photos/benedikt/Benedikt%20Scha%CC%88chner%20Download%20(1).jpeg',
+    width: 600,
+    height: 800,
+    alt: 'Weiteres Porträtfoto',
+    title: 'Porträt II',
+    description: 'Ein weiteres Porträt.',
+    aiHint: 'portrait man'
+  },
+  {
+    src: 'https://raw.githubusercontent.com/SchBenedikt/portfolio/master/src/app/photos/benedikt/Benedikt%20Scha%CC%88chner%20Download%20(2).jpeg',
+    width: 600,
+    height: 800,
+    alt: 'Benedikt Schächner im Freien',
+    title: 'Outdoor',
+    description: 'Foto im Freien aufgenommen.',
+    aiHint: 'person outdoor'
+  },
+  {
+    src: 'https://raw.githubusercontent.com/SchBenedikt/portfolio/master/src/app/photos/benedikt/Benedikt%20Scha%CC%88chner%20Download%20(3).jpeg',
+    width: 600,
+    height: 800,
+    alt: 'Porträtfoto von Benedikt Schächner',
+    title: 'Nahaufnahme',
+    description: 'Eine Nahaufnahme.',
+    aiHint: 'person closeup'
+  },
+  {
+    src: 'https://raw.githubusercontent.com/SchBenedikt/portfolio/master/src/app/photos/benedikt/Benedikt%20Scha%CC%88chner%20Download%20(4).jpeg',
+    width: 800,
+    height: 600,
+    alt: 'Benedikt Schächner bei einer Veranstaltung',
+    title: 'Veranstaltung',
+    description: 'Auf einer Veranstaltung.',
+    aiHint: 'person event'
+  },
+  {
+    src: 'https://raw.githubusercontent.com/SchBenedikt/portfolio/master/src/app/photos/benedikt/Benedikt%20Scha%CC%88chner%20Download.jpeg',
+    width: 600,
+    height: 800,
+    alt: 'Profilbild von Benedikt Schächner',
+    title: 'Profil',
+    description: 'Ein Profilbild.',
+    aiHint: 'person profile'
+  },
+  {
+    src: 'https://raw.githubusercontent.com/SchBenedikt/portfolio/master/src/app/photos/benedikt/Benedikt%20Scha%CC%88chner%20Suche.jpeg',
+    width: 800,
+    height: 600,
+    alt: 'Benedikt Schächner bei der Arbeit',
+    title: 'Arbeit',
+    description: 'Bei der Arbeit an einem Projekt.',
+    aiHint: 'person working'
+  },
+  {
+    src: 'https://raw.githubusercontent.com/SchBenedikt/portfolio/master/src/app/photos/benedikt/Benedikt%20Scha%CC%88chner.jpeg',
+    width: 600,
+    height: 800,
+    alt: 'Ein weiteres Foto von Benedikt Schächner',
+    title: 'Klassisch',
+    description: 'Ein klassisches Foto.',
+    aiHint: 'person classic'
+  },
+   {
+    src: 'https://raw.githubusercontent.com/SchBenedikt/portfolio/master/src/app/photos/benedikt/Download%20(1).jpeg',
+    width: 800,
+    height: 600,
+    alt: 'Benedikt Schächner in Aktion',
+    title: 'Aktion',
+    description: 'Ein dynamisches Bild in Aktion.',
+    aiHint: 'person action'
+  },
 ];
