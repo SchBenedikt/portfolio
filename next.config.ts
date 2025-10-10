cea33c3e589f16b491e68be265566ee27a57d04d
Benedikt Schächner <benedikt.schaechner@gmail.com>
2025-10-10 12:12:43 +0000

    entferne das mit dem bild logo/bild url etc. das echte logo ist https://

--- next.config.ts
+++ next.config.ts
@@ -71,6 +71,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'www.bayernjudo.de',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   devIndicators: false,
